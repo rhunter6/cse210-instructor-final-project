f07a25433d75bf7342d24090130e8c29e715feb2
Russell Hunter <RussellH@Computer.org>
2021-11-18 21:00:06 -0600

    Add Director class

--- space-trader/game/director.py
+++ space-trader/game/director.py
@@ -1,6 +1,16 @@
+
 class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+
+    Stereotype:
+        Controller
+    """
+
     def __init__(self):
+        """The class constructor."""
         pass
 
     def start_game(self):
+        """Start the game."""
         print("Game started")
