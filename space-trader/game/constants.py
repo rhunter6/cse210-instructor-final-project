e1164203e6c9ffe95502f56833b50c6993aaca51
Russell Hunter <RussellH@Computer.org>
2021-11-20 20:28:27 -0600

    Swtich to Gui buttons for menu view

--- space-trader/game/constants.py
+++ space-trader/game/constants.py
@@ -3,10 +3,11 @@
 # Window Constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Arcade Space Shooter"
+SCREEN_TITLE = "Space Trader"
 SCALING = 2.0
 
 # MenuView Constants
 FONT_SIZE_BIG = 50
 FONT_SIZE_MEDIUM = 25
+FONT_SIZE_SMALL = 15
 TEXT_PADDING = 20
--- space-trader/game/menu_view.py
+++ space-trader/game/menu_view.py
@@ -1,28 +1,70 @@
 import arcade
+import arcade.gui
 from game import constants
 
 
 class MenuView(arcade.View):
+    """
+    The starting menu for the game.  Allows the user to see instructions or start the game.
+
+    Stereotype:
+        User Interfacer
+    """
+
+    def __init__(self):
+        """
+        The class constructor.  This sets up the buttons on screen.
+        """
+        super().__init__()
+
+        # a UIManager to handle the UI.
+        self._manager = arcade.gui.UIManager()
+        self._manager.enable()
+
+        # Create a vertical BoxGroup to align buttons
+        self._v_box = arcade.gui.UIBoxLayout()
+
+        # Create the instruction button
+        instructions_button = arcade.gui.UIFlatButton(
+            text="Instructions", width=200)
+        instructions_button.on_click = self.on_click_instructions
+        self._v_box.add(instructions_button.with_space_around(bottom=20))
+
+        # Create the start game button
+        start_button = arcade.gui.UIFlatButton(text="Start Game", width=200)
+        start_button.on_click = self.on_click_start
+        self._v_box.add(start_button.with_space_around(bottom=20))
+
+        # Create a widget to hold the v_box widget, that will center the buttons
+        self._manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x",
+                anchor_y="center_y",
+                child=self._v_box)
+        )
+
+    def on_click_instructions(self, event):
+        """Show the instructions"""
+        print("Instructions")
+
+    def on_click_start(self, event):
+        """Start the game."""
+        print("Start Game")
+        arcade.exit()
+
     def on_show(self):
+        """Set the background color."""
         arcade.set_background_color(arcade.color.BLEU_DE_FRANCE)
 
     def on_draw(self):
+        """Draw the buttons"""
         arcade.start_render()
+
+        # Draw a title for the screen.
         line_y = constants.SCREEN_HEIGHT - \
             constants.FONT_SIZE_BIG - constants.TEXT_PADDING * 2
-        arcade.draw_text("Menu Screen", constants.SCREEN_WIDTH / 2, line_y,
+        arcade.draw_text("Menu View", constants.SCREEN_WIDTH / 2, line_y,
                          arcade.color.YELLOW, font_size=constants.FONT_SIZE_BIG, anchor_x="center")
 
-        line_y -= constants.FONT_SIZE_MEDIUM + constants.TEXT_PADDING * 2
-        arcade.draw_text("Instructions", constants.SCREEN_WIDTH / 2, line_y,
-                         arcade.color.WHITE, font_size=constants.FONT_SIZE_MEDIUM, anchor_x="center")
-
-        line_y -= constants.FONT_SIZE_MEDIUM + constants.TEXT_PADDING
-        arcade.draw_text("Start Game", constants.SCREEN_WIDTH / 2, line_y,
-                         arcade.color.WHITE, font_size=constants.FONT_SIZE_MEDIUM, anchor_x="center")
-
-    def on_mouse_press(self, _x, _y, _button, _modifiers):
-        # instructions_view = InstructionView()
-        # self.window.show_view(instructions_view)
-
-        arcade.exit()
+        # Draw the Gui
+        self._manager.draw()
